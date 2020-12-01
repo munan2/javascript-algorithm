735555c0307492c5de3528483488aa4d6e20fbc1
zhangyan19 <zhangyan19@100tal.com>
2020-12-02 00:41:44 +0800

    feat(*): 两数之和

---
+++ 力扣数组/08 两数之和-输入有序数组.js
@@ -0,0 +1,31 @@
+/*
+ * @Description: 
+  给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+  函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+
+  示例：
+  输入: numbers = [2, 7, 11, 15], target = 9
+  输出: [1,2]
+  解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ * @Version: 2.0
+ * @Autor: zhangyan
+ * @Date: 2020-12-02 00:30:59
+ * @LastEditors: zhangyan
+ * @LastEditTime: 2020-12-02 00:39:19
+ */
+var twoSum = function(numbers, target) {
+  let length = numbers.length
+  for (let i=0, j=length-1; i <= j; ) {
+    if (numbers[i] + numbers[j] === target) {
+      return [i+1, j+1]
+    } else if (numbers[i] + numbers[j] > target) {
+      j--
+    } else {
+      i++
+    }
+  }
+  return null
+};
+const arr = [2, 7, 11, 15]
+const res = twoSum(arr, 9)
+console.log('res', res)
---
+++ 力扣数组/09 验证回文串.js
@@ -0,0 +1,20 @@
+/*
+ * @Description: 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ 说明：本题中，我们将空字符串定义为有效的回文串。
+
+ 示例：
+  输入: "A man, a plan, a canal: Panama"
+  输出: true
+
+  输入: "race a car"
+  输出: false
+ * @Version: 2.0
+ * @Autor: zhangyan
+ * @Date: 2020-12-02 00:40:24
+ * @LastEditors: zhangyan
+ * @LastEditTime: 2020-12-02 00:41:24
+ */
+
+var isPalindrome = function(s) {
+
+};
